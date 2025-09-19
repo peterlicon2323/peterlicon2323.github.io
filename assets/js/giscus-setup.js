f419ff162ace43fa14296a0f39c205b2a15c81ae
peterlicon2323 <gnrg823nasdwq@onmail.com>
2025-09-19 05:15:22 +0000

    Deploying to gh-pages from @ peterlicon2323/peterlicon2323.github.io@58710b422c3f88dcb61d527a313e29005e0c77d1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -5,11 +5,11 @@ function determineGiscusTheme() {
       document.documentElement.getAttribute("data-theme") ||
       "system";
 
-    if (theme === "dark") return "dark";
-    if (theme === "light") return "light";
+    if (theme === "dark") return "algolia";
+    if (theme === "light") return "buefy";
 
     const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    return prefersDark ? "dark" : "light";
+    return prefersDark ? "algolia" : "buefy";
   
 }
 
