74b22b973fba7e52dc3079440e02c8a17f922dbe
peterlicon2323 <gnrg823nasdwq@onmail.com>
2025-09-20 11:16:09 +0000

    Deploying to gh-pages from @ peterlicon2323/peterlicon2323.github.io@6a846be704a1c72b22b6a59aa6910bddf12e8355 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,7 +430,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-gradio-test",
+          title: 'Gradio Test',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
@@ -475,11 +480,6 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "projects-gradio-test",
-          title: 'Gradio Test',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/gradio_test/";
             },},{
         id: 'social-email',
         title: 'email',
